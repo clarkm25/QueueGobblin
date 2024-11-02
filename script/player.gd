8da006811a03e2314a22919753f48e61a76cae4a
HarryThVu <vuh25@up.edu>
2024-11-01 20:36:57 -0700

    Basic player movement

---
+++ script/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+const move_speed = 300.0
+
+
+func _physics_process(delta):
+	var direction = Input.get_vector("move_left","move_right","move_up", "move_down")
+	if direction:
+		velocity = direction * move_speed
+	else:
+		velocity.x = move_toward(velocity.x,0,move_speed)
+		velocity.y = move_toward(velocity.y,0,move_speed)
+		
+	move_and_slide()
