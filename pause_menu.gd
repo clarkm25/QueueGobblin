373d71f67b8b96ac542b8098db611152900b5594
ngDaniel025 <nguyenda25@up.edu>
2024-11-02 14:00:36 -0700

    added pause menu and script

---
+++ pause_menu.gd
@@ -0,0 +1,28 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func resume():
+	get_tree().paused = true
+	$AnimationPlayer.play_backwards("blur")
+	
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+	
+func testEsc():
+	if Input.is_action_just_pressed("esc") and get_tree().paused == false:
+		pause()
+	elif Input.is_action_just_pressed("esc") and get_tree().paused == true:
+		resume()
+
+
+func _on_resume_pressed() -> void:
+	resume()
+
+
+func _on_restart_pressed() -> void:
+	get_tree().reload_current_scene()
+
+func _on_quit_pressed() -> void:
+	get_tree().quit
