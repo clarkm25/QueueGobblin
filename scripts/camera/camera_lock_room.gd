41d4ef60ca93d771bb72ea8f1600e62815543048
PennyS8 <silliman25@up.edu>
2024-11-02 15:22:47 -0700

    completed camera locking

--- scripts/camera/camera_lock_room.gd
+++ scripts/camera/camera_lock_room.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
-onready var camera_bedroom = $Camera2DGameRoom
-onready var player = $Player
+@onready var camera_capture = $CameraCapture
+
+signal capture
+signal release
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,3 +13,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_camera_capture_area_entered(area: Area2D) -> void:
+	emit_signal("capture", self)
