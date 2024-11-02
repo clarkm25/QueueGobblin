805d4cd83331152bd0b95f825f1beb8a7577e1d0
ngDaniel025 <nguyenda25@up.edu>
2024-11-02 15:00:35 -0700

    Fixed pause menu openning
    Background blur during pause doesn't work. Not removing code for now because I am unsure how important it is. Works as expected

--- scripts/UI/pause/pause_menu.gd
+++ scripts/UI/pause/pause_menu.gd
@@ -3,17 +3,19 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func resume():
-	get_tree().paused = true
+	get_tree().paused = false
 	$AnimationPlayer.play_backwards("blur")
+	hide()
 	
 func pause():
 	get_tree().paused = true
 	$AnimationPlayer.play("blur")
+	show()
 	
-func testEsc():
-	if Input.is_action_just_pressed("pause") and get_tree().paused == false:
+func _input(event):
+	if event.is_action_pressed("pause") and get_tree().paused == false:
 		pause()
-	elif Input.is_action_just_pressed("pause") and get_tree().paused == true:
+	elif event.is_action_pressed("pause") and get_tree().paused == true:
 		resume()
 
 func _on_resume_pressed() -> void:
