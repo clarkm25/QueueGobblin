4de4675fe53db4100d669f866be79a51a5eded54
PennyS8 <silliman25@up.edu>
2024-11-02 20:26:29 -0700

    started adding menus to gamer_home

--- scripts/camera/camera_lock.gd
+++ scripts/camera/camera_lock.gd
@@ -2,26 +2,38 @@ extends Node2D
 
 @onready var camera = $Camera2D
 @onready var player = $Player
+@onready var timer = $GameTimer
 
+var replay_main_scene := preload("res://scenes/level.tscn")
 var target_pos = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	# Camera locking setup
 	target_pos = player.global_position
-	
 	for room in get_tree().get_nodes_in_group("room"):
 		room.capture.connect(set_camera)
+	
+	# Game timer setup
+	timer.one_shot = true
+	timer.autostart = false
+	timer.wait_time = 60 # Duration of game phase
+	timer.start()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	# Camera follows player through the hallway
 	if player.global_position.y > (8*16) and player.global_position.y < (13*16):
 		target_pos.x = player.global_position.x
-	
+	# Camera tween when switching between camera locks
 	var tween = get_tree().create_tween()
 	tween.tween_property(camera, "global_position", target_pos, 0.25)
 
 
 func set_camera(room : Node2D):
 	target_pos = room.global_position
+
+
+func _on_game_timer_timeout() -> void:
+	# temp functionality of the timer
+	replay_main_scene.instantiate()
